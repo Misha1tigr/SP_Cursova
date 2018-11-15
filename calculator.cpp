d76906fa9d514a415aea930517e8def636d446ad
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Merged in multiplication operation
    Signed-off-by: Mykhailo Korbut <korbutmykhailo@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -8,4 +8,8 @@ int Calculator::Add (double a, double b)
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
 }
